1e5b1b105af91f7f6697d61758e5ba2a053e6e9c
kuangyu0801 <57182723+kuangyu0801@users.noreply.github.com>
2021-07-09 16:52:35 +0200

    Add third TDD cases

--- src/test/java/MovieStoreTest.java
+++ src/test/java/MovieStoreTest.java
@@ -5,6 +5,7 @@ import java.util.List;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.containsInAnyOrder;
 
 public class MovieStoreTest {
 
@@ -26,5 +27,20 @@ public class MovieStoreTest {
         List<Movie> results = movieStore.findByPartialTitle("arry");
         assertThat(results.size(), is(1));
         assertThat(results, contains(harryPotter));
+    }
+
+    @Test
+    public void findMoviesWhenTitleMatches() throws Exception {
+        MovieStore movieStore = new MovieStore();
+        Movie harryPotter = new Movie("Harry Potter");
+        movieStore.add(harryPotter);
+        Movie startwars = new Movie("Star Wars");
+        movieStore.add(startwars);
+        Movie startrek = new Movie("STAR Trek");
+        movieStore.add(startrek);
+        movieStore.add(new Movie("Hobbit"));
+        List<Movie> results = movieStore.findByPartialTitle("tar");
+        assertThat(results.size(), is(2));
+        assertThat(results, containsInAnyOrder(startrek, startwars));
     }
 }
