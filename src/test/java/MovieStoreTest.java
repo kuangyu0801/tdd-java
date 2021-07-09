d0154ed04b43796c682f669722823fff08d94e15
kuangyu0801 <57182723+kuangyu0801@users.noreply.github.com>
2021-07-09 17:01:51 +0200

    Refactor Test Class

--- src/test/java/MovieStoreTest.java
+++ src/test/java/MovieStoreTest.java
@@ -1,3 +1,4 @@
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.List;
@@ -8,22 +9,28 @@ import static org.hamcrest.Matchers.contains;
 import static org.hamcrest.Matchers.containsInAnyOrder;
 
 public class MovieStoreTest {
+    MovieStore movieStore = new MovieStore();
+    private final Movie harryPotter = new Movie("Harry Potter");
+    private final Movie startwars = new Movie("Star Wars");
+    private final Movie startrek = new Movie("STAR Trek");
+    private final Movie hobbit = new Movie("Hobbit");
+
+    @Before
+    public void setUp() throws Exception {
+        movieStore.add(harryPotter);
+        movieStore.add(startwars);
+        movieStore.add(startrek);
+        movieStore.add(hobbit);
+    }
 
     @Test
     public void returnNoResultWhenNoTitle() throws Exception {
-        MovieStore movieStore = new MovieStore();
         List<Movie> results = movieStore.findByPartialTitle("abc");
         assertThat(results.size(), is(0));
     }
 
     @Test
     public void findAMovieWhenTitleMatches() throws Exception {
-        MovieStore movieStore = new MovieStore();
-        Movie harryPotter = new Movie("Harry Potter");
-        movieStore.add(harryPotter);
-        movieStore.add(new Movie("Star Wars"));
-        movieStore.add(new Movie("Star Trek"));
-        movieStore.add(new Movie("Hobbit"));
         List<Movie> results = movieStore.findByPartialTitle("arry");
         assertThat(results.size(), is(1));
         assertThat(results, contains(harryPotter));
@@ -31,14 +38,7 @@ public class MovieStoreTest {
 
     @Test
     public void findMoviesWhenTitleMatches() throws Exception {
-        MovieStore movieStore = new MovieStore();
-        Movie harryPotter = new Movie("Harry Potter");
-        movieStore.add(harryPotter);
-        Movie startwars = new Movie("Star Wars");
-        movieStore.add(startwars);
-        Movie startrek = new Movie("STAR Trek");
-        movieStore.add(startrek);
-        movieStore.add(new Movie("Hobbit"));
+
         List<Movie> results = movieStore.findByPartialTitle("tar");
         assertThat(results.size(), is(2));
         assertThat(results, containsInAnyOrder(startrek, startwars));
